7fd4e79f6c313485889588a6f37a8fe8c8b441cc
lprieri <lprieri@student.codaml.nl>
2024-07-15 16:38:34 +0200

    Relocated print util for arrays.c

---
+++ src/print/arrays.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   arrays.c                                           :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: lprieri <lprieri@student.codam.nl>           +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2024/07/03 15:41:01 by lprieri       #+#    #+#                 */
+/*   Updated: 2024/07/10 20:44:44 by lprieri       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../include/minishell.h"
+
+void	ft_print_2d_arr(char **arr)
+{
+	int	i;
+
+	if (!arr || !*arr)
+	{
+		ft_putstr_fd((char *) get_error_msg(ERR_NULL), STDERR_FILENO);
+		return ;
+	}
+	i = 0;
+	while (arr[i])
+	{
+		ft_printf("%s\n", arr[i]);
+		i++;
+	}
+}
+
+void	ft_print_3d_arr(char ***arr)
+{
+	int	i;
+	int	j;
+
+	if (!arr || !*arr)
+	{
+		ft_putstr_fd((char *) get_error_msg(ERR_NULL), STDERR_FILENO);
+		return ;
+	}
+	i = 0;
+	while (arr[i])
+	{
+		j = 0;
+		while (arr[j])
+		{
+			ft_printf("%s\n", arr[i][j]);
+			j++;
+		}
+		i++;
+	}
+}
