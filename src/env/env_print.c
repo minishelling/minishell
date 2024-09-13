871eb0c66607c6ff3ea2e33addcd87b9395b503a
Lisandro Prieri <lprieri@student.codam.nl>
2024-09-13 17:54:19 +0200

    Adding env_print_node.

--- src/env/env_print.c
+++ src/env/env_print.c
@@ -1,5 +1,26 @@
 #include "../../include/minishell.h"
 
+void	env_print_node(t_env *node)
+{
+	if (!node)
+		return ;
+	else
+	{
+		if (node->keyvalue)
+			ft_printf("Key-value: %s\n", node->keyvalue);
+		else
+			ft_printf("No key-value\n");
+		if (node->key)
+			ft_printf("Key: %s\n", node->key);
+		else
+			ft_printf("No key\n");
+		if (node->value)
+			ft_printf("Value: %s\n\n\n", node->value);
+		else
+			ft_printf("No value\n");
+	}
+}
+
 void	env_print_list(t_env *head)
 {
 	printf ("Lisandro's\n");
