7bcabc988c36fab53cdd1e2a24617bfbb296e9a7
Lisandro Prieri <lprieri@student.codam.nl>
2024-09-19 21:07:51 +0200

    Adding export function. Seems to be working well, but when there's a pipe after export it still prints the whole thing.

--- src/builtins/export.c
+++ src/builtins/export.c
@@ -2,7 +2,6 @@
 
 int	export_builtin(t_shell *shell, char **args)
 {
-	t_env	*node;
 	char	*key;
 	char	*value;
 	t_ecode	status;
@@ -14,10 +13,10 @@ int	export_builtin(t_shell *shell, char **args)
 	key = get_key_from_keyvalue(args[1]);
 	if (!key)
 		return (MALLOC_ERROR);
-	status = get_value_from_keyvalue(args[1]);
-	if (!value)
-		return (MALLOC_ERROR);
-	update_env_node(shell->env_list, )
-
+	value = NULL;
+	status = get_value_from_keyvalue(args[1], &value);
+	if (status != SUCCESS && status != NULL_STRING)
+		return (ft_free((void **) &key), MALLOC_ERROR);
+	update_env_node(&shell->env_list, key, value, true);
 	return (SUCCESS);
 }
