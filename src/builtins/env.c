d1c207eb2494b6093aaa15cbff0639953121ef7b
Lisandro Prieri <lprieri@student.codam.nl>
2024-09-23 17:35:08 +0200

    Added env builtin.

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -4,7 +4,11 @@ t_ecode	env_builtin(t_shell *shell, char **cmd_args)
 {
 	if (!shell || !shell->env_list)
 		return (NULL_ERROR);
-	(void) cmd_args;
+	if (cmd_args[1])
+	{
+		ft_putstr_fd("env: options and arguments are not implemented.\n", 2);
+		return (FAILURE);
+	}
 	print_env(shell->env_list);
 	return (SUCCESS);
 }
