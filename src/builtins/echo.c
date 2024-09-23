9aaf5aa4f20e2eafabc796c71d3d2fb33342b9da
Lisandro Prieri <lprieri@student.codam.nl>
2024-09-23 11:27:27 +0200

    Fixed echo. It doens't print backslash characters anymore.

--- src/builtins/echo.c
+++ src/builtins/echo.c
@@ -1,12 +1,12 @@
 #include "../../include/minishell.h"
 
-t_ecode echo_builtin(t_shell *shell, char **cmd_args) //Return unsigned int8_t?
+t_ecode echo_builtin(t_shell *shell, char **cmd_args)
 {
 	bool	newline;
 	char	**str_arr;
 	int		i;
+	int		j;
 
-	//printf("We're running echo\n");
 	(void) shell;
 	if (!cmd_args)
 		return (FAILURE);
@@ -28,7 +28,13 @@ t_ecode echo_builtin(t_shell *shell, char **cmd_args) //Return unsigned int8_t?
 	i = 0;
 	while (str_arr[i])
 	{
-		ft_putstr_fd(str_arr[i], 1);
+		j = 0;
+		while (str_arr[i][j])
+		{
+			if (str_arr[i][j] != '\\')
+				ft_putchar_fd(str_arr[i][j], 1);
+			j++;
+		}
 		if (str_arr[i + 1])
 			ft_putchar_fd(' ', 1);
 		i++;
