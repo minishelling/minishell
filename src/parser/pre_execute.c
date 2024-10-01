7669f1dff8db04200d5e14896956de68d0f3faff
Lisandro Prieri <lprieri@student.codam.nl>
2024-10-01 20:00:02 +0200

    Replaced tree_node with parent_tree_node in the 3rd argument of pre_execute inside the ANR_OP and OR_OP conditions. Also added the older version of those conditions back (as comments).

--- src/parser/pre_execute.c
+++ src/parser/pre_execute.c
@@ -11,12 +11,12 @@ int pre_execute(t_shell *shell, t_tree *tree_node, t_tree *parent_tree_node, int
     if (tree_node == NULL)
         return exit_code;
 
-    if (tree_node->type == T_PIPE)
-    {
-        printf("We're going in T_PIPE handling case\n");
-        return(handle_pipe_subtree(shell, tree_node));
-    }
-    else if (tree_node->left != NULL)
+	if (tree_node->type == T_PIPE)
+	{
+		printf("We're going in T_PIPE handling case\n");
+		return (handle_pipe_subtree(shell, tree_node));
+	}
+	if (tree_node->left != NULL)
     {
         parent_tree_node = tree_node;
         exit_code = pre_execute(shell, tree_node->left, parent_tree_node, exit_code);
@@ -46,27 +46,35 @@ int pre_execute(t_shell *shell, t_tree *tree_node, t_tree *parent_tree_node, int
     }
     if (parent_tree_node && parent_tree_node->type == T_AND_OPR && exit_code == 0) 
     {
-		// printf ("performing the right side of AND\n");
+		printf ("performing the right side of AND\n");
 		if (tree_node->right)
 		{
 			if (tree_node->right->type == T_PIPE)
 				// return 1;
 				return (handle_pipe_subtree(shell, tree_node));
 			else
-				return pre_execute(shell, parent_tree_node->right, tree_node, exit_code);
+				return pre_execute(shell, parent_tree_node->right, parent_tree_node, exit_code);
 		}
+
+		//Old version
+		// if (tree_node->right)
+		// 	return pre_execute(shell, parent_tree_node->right, parent_tree_node, exit_code);
 	}
-    else if (parent_tree_node && parent_tree_node->type == T_OR_OPR && exit_code != 0) 
+    else if (parent_tree_node && parent_tree_node->type == T_OR_OPR && exit_code != 0)
     {
-        // printf ("performing the right side of OR");
+        printf ("performing the right side of OR");
         if (tree_node->right)
         {
 			if (tree_node->right->type == T_PIPE)
 				// return 1;
 				return (handle_pipe_subtree(shell, tree_node));
 			else
-				return pre_execute(shell, parent_tree_node->right, tree_node, exit_code);
+				return pre_execute(shell, parent_tree_node->right, parent_tree_node, exit_code);
 		}
-    }
+
+		//Old version
+		// if (tree_node->right)
+		// 	return pre_execute(shell, parent_tree_node->right, parent_tree_node, exit_code);
+	}
     return exit_code;
 }
