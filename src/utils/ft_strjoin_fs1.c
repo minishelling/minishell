7908c2082ab6a6adb0a942b15f44e9c6142471b9
lprieri <lisandroprieri@gmail.com>
2024-10-18 21:48:47 +0200

    Modified the return variable of safe_assign_str, added an error message on malloc failure, and added documentation to both functions in the file.

--- src/utils/ft_strjoin_fs1.c
+++ src/utils/ft_strjoin_fs1.c
@@ -1,5 +1,17 @@
 #include "../../include/minishell.h"
 
+/**
+ * @brief Returns the concatenation of s1 and s2,
+ * while also freeing the first string in the process.
+ * 
+ * @param s1 A pointer to the first string.
+ * @param s2 The second string.
+ * 
+ * @return If the first string is NULL, or there's a malloc failure
+ * it returns NULL and in the latter case it prints an error message.
+ * Otherwise it returns a new string that is the product of
+ * concatenating s1 and s2.
+ */
 char	*ft_strjoin_fs1(char **s1, char const *s2)
 {
 	char	*dst;
@@ -14,18 +26,31 @@ char	*ft_strjoin_fs1(char **s1, char const *s2)
 	ls2 = ft_strlen(s2);
 	dst = ft_calloc(ls1 + ls2 + 1, sizeof(char));
 	if (!dst)
-		return (ft_free((void **) s1), NULL);
+	{
+		handle_perror("ft_strjoin_fs1");
+		ft_free((void **) s1);
+		return (NULL);
+	}
 	ft_strlcpy(dst, *s1, (ls1 + 1));
 	ft_strlcat(dst, s2, (ls1 + ls2 + 1));
 	ft_free((void **) s1);
 	return (dst);
 }
 
-int	safe_assign_str(char **dest, const char *src)
+/**
+ * @brief Duplicates the string src into dest,
+ * freeing dest if it held any value beforehand.
+ * 
+ * @param dest A pointer to the destination string.
+ * @param src The string whose value we want to duplicate.
+ * 
+ * @return On malloc failure it prints an error message
+ * and returns an error code. On success it returns SUCCESS.
+ */
+t_ecode	safe_assign_str(char **dest, const char *src)
 {
 	if (*dest)
 	{
-		//printf ("freeing %p and setting to NULL\n", *dest);
 		free(*dest);
 		*dest = NULL;
 	}
@@ -33,7 +58,7 @@ int	safe_assign_str(char **dest, const char *src)
 	{
 		*dest = ft_strdup(src);
 		if (*dest == NULL)
-			return(ERR_MEM);
+			return (handle_perror("safe_assign_str"), ERR_MEM);
 	}
 	else
 		(*dest) = NULL;
