2b9322b95bce53c86338042b33a1a02c3eb14c27
lprieri <lisandroprieri@gmail.com>
2024-10-19 15:05:15 +0200

    Added documentation to redirections

--- src/executor/pipe_subtree.c
+++ src/executor/pipe_subtree.c
@@ -1,37 +1,54 @@
 #include "../../include/minishell.h"
 
-static void close_fds(int fd1, int fd2)
+static int	handle_pipe_left_node(t_shell *shell, t_tree *tree_node, int fd[2]);
+static int	handle_pipe_right_node(t_shell *shell, t_tree *tree_node, int fd[2]);
+static void close_fds(int fd1, int fd2);
+
+int	handle_pipe_subtree(t_shell *shell, t_tree *tree_node)
 {
-	if (fd1 != -1)
-	{
-		if (close(fd1) == -1)
-			perror("close fd1");
-	}
-	if (fd2 != -1)
-	{
-		if (close(fd2) == -1)
-			perror("close fd2");
-	}
+	int		fd[2];
+	int		status;
+	pid_t	left_node_pid;
+	pid_t	right_node_pid;
+
+	if (pipe(fd) == ERROR)
+		exit(EXIT_FAILURE);
+	left_node_pid = fork();
+	if (left_node_pid == ERROR)
+		exit(EXIT_FAILURE);
+	else if (left_node_pid == 0)
+		handle_pipe_left_node(shell, tree_node, fd);
+	waitpid(left_node_pid, &status, 0);
+	right_node_pid = fork();
+	if (right_node_pid == ERROR)
+		exit(EXIT_FAILURE);
+	else if (right_node_pid == 0)
+		handle_pipe_right_node(shell, tree_node, fd);
+	close_fds(fd[READ_END], fd[WRITE_END]);
+	waitpid(right_node_pid, &status, 0);
+	while (wait(NULL) != ERROR)
+		;
+	return (status);
 }
 
 static int	handle_pipe_left_node(t_shell *shell, t_tree *tree_node, int fd[2])
 {
 	int	status;
 
-	if (dup2(fd[WRITE_END], STDOUT_FILENO) == -1)
+	if (dup2(fd[WRITE_END], STDOUT_FILENO) == ERROR)
 	{
 		perror("dup2");                                                                                         
 		exit(EXIT_FAILURE);
 	}
 	close_fds(fd[READ_END], fd[WRITE_END]);
 	status = pre_execute(shell, tree_node->left, tree_node, 0);
-	exit(status); //Should probably take another variable as arg, and pass the return of pre_execute there.
+	exit(status);
 }
 
 static int	handle_pipe_right_node(t_shell *shell, t_tree *tree_node, int fd[2])
 {
 	int	status;
-	if (dup2(fd[READ_END], STDIN_FILENO) == -1)
+	if (dup2(fd[READ_END], STDIN_FILENO) == ERROR)
 	{
 		perror("dup2");
 		exit(EXIT_FAILURE);
@@ -41,29 +58,16 @@ static int	handle_pipe_right_node(t_shell *shell, t_tree *tree_node, int fd[2])
 	exit(status);
 }
 
-int	handle_pipe_subtree(t_shell *shell, t_tree *tree_node)
+static void close_fds(int fd1, int fd2)
 {
-	int		fd[2];
-	int		status;
-	pid_t	left_node_pid;
-	pid_t	right_node_pid;
-
-	if (pipe(fd) == -1)
-		exit(EXIT_FAILURE);
-	left_node_pid = fork();
-	if (left_node_pid == -1)
-		exit(EXIT_FAILURE);
-	else if (left_node_pid == 0)
-		handle_pipe_left_node(shell, tree_node, fd);
-	waitpid(left_node_pid, &status, 0);
-	right_node_pid = fork();
-	if (right_node_pid == -1)
-		exit(EXIT_FAILURE);
-	else if (right_node_pid == 0)
-		handle_pipe_right_node(shell, tree_node, fd);
-	close_fds(fd[READ_END], fd[WRITE_END]);
-	waitpid(right_node_pid, &status, 0);
-	while (wait(NULL) != -1)
-		;
-	return (status);
+	if (fd1 != ERROR)
+	{
+		if (close(fd1) == ERROR)
+			perror("close fd1");
+	}
+	if (fd2 != ERROR)
+	{
+		if (close(fd2) == ERROR)
+			perror("close fd2");
+	}
 }
--- src/executor/redirections.c
+++ src/executor/redirections.c
@@ -1,6 +1,56 @@
 #include "../../include/minishell.h"
 
-static t_ecode	open_current_redir(t_redir_id redir_id, char *redir_file, int *fd)
+static t_ecode	open_current_redir(t_redir_id redir_id,
+					char *redir_file, int *fd);
+static t_ecode	replace_redir_fd(t_cmd *cmd, t_redir *redir);
+static t_ecode	close_and_replace(int replacement, int *oldfd);
+
+/**
+ * @brief Opens all input and output redirections of a command.
+ * 
+ * @param shell A pointer to the shell structure.
+ * @param current_cmd A pointer to the structure of the current command.
+ * 
+ * @return If a redirection fails to open, if there's a dup failure,
+ * or if the structures were NULL in the first place, it returns FAILURE,
+ * otherwise it returns SUCCESS.
+ */
+t_ecode	open_redirections(t_shell *shell, t_cmd *current_cmd)
+{
+	t_redir	*current_redir;
+
+	if (!shell || !current_cmd)
+		return (FAILURE);
+	current_redir = current_cmd->redir;
+	while (current_redir)
+	{
+		if (open_current_redir(current_redir->redir_id,
+				current_redir->file, &current_redir->fd) != SUCCESS)
+			return (FAILURE);
+		if (replace_redir_fd(current_cmd, current_redir) != SUCCESS)
+			return (FAILURE);
+		current_redir = current_redir->next;
+	}
+	return (SUCCESS);
+}
+
+/**
+ * @brief Opens the current redirection.
+ * 
+ * It checks what kind of redirection it is and opens the file
+ * appropriately. If it is a HEREDOC, instead of opening a file
+ * it converts the string to an integer, since the file with
+ * the heredoc contents it's already open and unlinked.
+ * 
+ * @param redir_id The type of input or output redirection.
+ * @param redir_file The redirection file to be opened.
+ * @param fd The file descriptor in which to store the open value.
+ * 
+ * @return If the file descriptor is ERROR (-1), it prints an error message
+ * and returns FAILURE. Otherwise it returns SUCCESS.
+ */
+static t_ecode	open_current_redir(t_redir_id redir_id,
+					char *redir_file, int *fd)
 {
 	if (!redir_file)
 		return (SUCCESS);
@@ -12,7 +62,7 @@ static t_ecode	open_current_redir(t_redir_id redir_id, char *redir_file, int *fd
 		*fd = open(redir_file, O_WRONLY | O_TRUNC | O_CREAT, 0644);
 	else if (redir_id == APP)
 		*fd = open(redir_file, O_WRONLY | O_APPEND | O_CREAT, 0644);
-	if (*fd == -1)
+	if (*fd == ERROR)
 	{
 		handle_perror(redir_file);
 		return (FAILURE);
@@ -20,21 +70,28 @@ static t_ecode	open_current_redir(t_redir_id redir_id, char *redir_file, int *fd
 	return (SUCCESS);
 }
 
-static t_ecode	close_and_replace(int replacement, int *oldfd)
-{
-	if (close(*oldfd) == -1)
-		handle_perror("close_and_replace");
-	*oldfd = replacement;
-	return (FAILURE);
-}
-
+/**
+ * @brief Replaces the 'latest_in' 'latest_out' redirection variables
+ * with the current redirection.
+ * 
+ * 
+ * 
+ * @param cmd A pointer to the current command's structure.
+ * @param redir A pointer to the current command's redirection structure.
+ * 
+ * @return If the current redirection is ERROR (-1), it closes the latest
+ * redirection and replaces it with the error number, and returns FAILURE.
+ * Otherwise it will attempt to duplicate the current redirection into the
+ * 'latest' variable, and close the current redirection file descriptor
+ * afterwards, returning SUCCESS. On error it returns FAILURE.
+ */
 static t_ecode	replace_redir_fd(t_cmd *cmd, t_redir *redir)
 {
-	int	status = SUCCESS;
+	t_ecode	status;
 
 	if (redir->redir_id == HERE || redir->redir_id == IN)
 	{
-		if (redir->fd == -1)
+		if (redir->fd == ERROR)
 			return (close_and_replace(redir->fd, &cmd->latest_in));
 		else if (cmd->latest_in == STDIN_FILENO)
 			cmd->latest_in = redir->fd;
@@ -43,7 +100,7 @@ static t_ecode	replace_redir_fd(t_cmd *cmd, t_redir *redir)
 	}
 	else if (redir->redir_id == OUT || redir->redir_id == APP)
 	{
-		if (redir->fd == -1)
+		if (redir->fd == ERROR)
 			return (close_and_replace(redir->fd, &cmd->latest_out));
 		else if (cmd->latest_out == STDOUT_FILENO)
 			cmd->latest_out = redir->fd;
@@ -55,20 +112,25 @@ static t_ecode	replace_redir_fd(t_cmd *cmd, t_redir *redir)
 	return (SUCCESS);
 }
 
-t_ecode	open_redirections(t_shell *shell, t_cmd *current_cmd)
+/**
+ * @brief It closes the oldfd and replaces it the the replacement.
+ * 
+ * @param replacement The fd to replace into oldfd.
+ * @param oldfd A pointer to the oldfd that we want to close and replace.
+ * 
+ * @return If oldfd after being replaced is ERROR (-1) it returns FAILURE,
+ * (which is probably needed as an error code for the calling function)
+ * otherwise it returns SUCCESS.
+ */
+static t_ecode	close_and_replace(int replacement, int *oldfd)
 {
-	t_redir	*current_redir;
-
-	if (!shell || !current_cmd)
-		return (NULL_ERROR);
-	current_redir = current_cmd->redir;
-	while (current_redir)
+	if (*oldfd != ERROR)
 	{
-		if (open_current_redir(current_redir->redir_id, current_redir->file, &current_redir->fd) != SUCCESS)
-			return (FAILURE);
-		if (replace_redir_fd(current_cmd, current_redir) != SUCCESS)
-			return (FAILURE);
-		current_redir = current_redir->next;
+		if (close(*oldfd) == ERROR)
+			handle_perror("close_and_replace");
 	}
+	*oldfd = replacement;
+	if (*oldfd == ERROR)
+		return (FAILURE);
 	return (SUCCESS);
 }
