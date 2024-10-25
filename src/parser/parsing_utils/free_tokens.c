4f6602c740cc8cdf5c9c2f257b10b416faec2442
Tamar Feuer <tfeuer@f1r4s19.codam.nl>
2024-10-25 12:35:10 +0200

    Change free_token to make sense, and remove redundant free(*token_list); *token_list = NULL; from free_token_list

--- src/parser/parsing_utils/free_tokens.c
+++ src/parser/parsing_utils/free_tokens.c
@@ -1,35 +1,17 @@
 #include "../../../include/minishell.h"
 
-void free_token(t_token **token)
+void	free_token(t_token **token)
 {
-	if (token && *token)
+	if (!token || !*token)
+		return;
+	if ((*token)->str)
 	{
-		if ((*token)->str)
-		{
-			free((*token)->str);
-			(*token)->str = NULL;
-		}
-		else
-		{
-			free(*token);
-			*token = NULL;
-		}
+		free((*token)->str);
+		(*token)->str = NULL;
 	}
+	free(*token);
+	*token = NULL;
 }
-
-
-// void	free_token(t_token **token)
-// {
-// 	if (!token && !*token)
-// 	return ;
-// 	if ((*token)->str)
-// 	{
-// 		free((*token)->str);
-// 		(*token)->str = NULL;
-// 	}
-// 	free(*token);
-// 	*token = NULL;
-// }
 
 void	free_token_list(t_token **token_list)
 {
@@ -45,6 +27,4 @@ void	free_token_list(t_token **token_list)
 		free_token(&current);
 		current = next;
 	}
-	free(*token_list);
-	*token_list = NULL;
 }
